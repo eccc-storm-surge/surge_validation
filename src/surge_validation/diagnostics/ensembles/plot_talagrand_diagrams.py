37c3923351a7725529286067025bf5ce50e02e50
Oleksandr Huziy <oleksandr.huziy@canada.ca>
2021-10-19 13:33:09 +0000

    Draft to calculate errorbars for Talagrand plots

--- src/surge_validation/diagnostics/ensembles/plot_talagrand_diagrams.py
+++ src/surge_validation/diagnostics/ensembles/plot_talagrand_diagrams.py
@@ -1,12 +1,14 @@
-from collections import OrderedDict
+from collections import OrderedDict, deque
 from concurrent.futures import as_completed
+import enum
 from pathlib import Path
 
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 from matplotlib.gridspec import GridSpec
 from matplotlib.ticker import MaxNLocator
+from numpy.core.fromnumeric import size
+from scipy.stats.stats import rankdata
 
 from surge_validation.detiding_validation import io_manager
 from surge_validation.detiding_validation.config import default_params
@@ -14,6 +16,20 @@ from surge_validation.misc.scheduling import get_thread_pool_executor, get_proce
 from surge_validation.utils import log_utils
 from surge_validation.utils.io_utils import cleanup
 from surge_validation.utils.strutils import stname_to_fname2
+from resample.bootstrap import confidence_interval
+import pandas as pd
+from functools import partial
+
+
+def ranks_to_probability(ranks: pd.DataFrame, bins, data_colname=io_manager.OBS_COL_NAME):
+    """Convert ranks to probabilities"""
+
+    
+    # left limit inclusive and the right excluded i.e [lead1, lead2)
+    
+    probability, bin_edges = np.histogram(ranks[data_colname], density=True, bins=bins)
+
+    return probability
 
 
 def plot_ranks_for_station(stname, bins, leads, rank_data, lbl_to_color: dict, plot_params: dict = None):
@@ -26,6 +42,8 @@ def plot_ranks_for_station(stname, bins, leads, rank_data, lbl_to_color: dict, p
     :param rank_data:
     :param lbl_to_color:
     :param plot_params:
+    :param ci_limits dictionary containing confidence interval limits {"min": {}, "max": {}}, the
+           dicts corresponding to the min and max keys have the same layout as rank_data: {syslabel: dataframe}
     :return: figure object
     """
     logger = log_utils.get_logger(__name__)
@@ -55,16 +73,26 @@ def plot_ranks_for_station(stname, bins, leads, rank_data, lbl_to_color: dict, p
 
         ax.hlines(1. / nbins, 1, nbins, color="magenta", lw=2)
 
-        for lbl, ranks in rank_data.items():
+        for lbl, probability in rank_data[lead_pair].items():
 
-            # left limit inclusive and the right excluded i.e [lead1, lead2)
-            vh = ranks[io_manager.VALIDH_COL_NAME]
-            current_lead_ranks = ranks[(vh >= lead_pair[0]) & (vh < lead_pair[1])]
+            
 
-            probability, bin_edges = np.histogram(current_lead_ranks, density=True, bins=bins)
-
-            ax.plot(range(1, nbins), probability, "o", label=lbl, c=lbl_to_color[lbl],
+            ax.plot(range(1, nbins), probability[io_manager.OBS_COL_NAME], "o", label=lbl, c=lbl_to_color[lbl],
                     markersize=plot_params["marker_size"], alpha=plot_params["alpha"])
+
+            # plot error bars
+            if "ci_min" in probability.columns:
+
+                p_min = probability["ci_min"]
+                p_max = probability["ci_max"]
+
+                assert ((p_min <= probability) & (probability <= p_max)).all()
+
+                yerr = np.array([probability - p_min, p_max - probability])
+
+                logger.info("p_min=%s\nprob=%s\np_max=%s\n", p_min, probability, p_max)
+                ax.errorbar(range(1, nbins), probability, yerr=yerr, c=lbl_to_color[lbl], alpha=plot_params["alpha"])
+                
 
         if i == 0:
             ax.legend(title=plot_params.get("legend_title", ""), loc="upper center")
@@ -73,11 +101,35 @@ def plot_ranks_for_station(stname, bins, leads, rank_data, lbl_to_color: dict, p
     return fig
 
 
+
+
+def prob_calc(s, rank_method="first", bins=None, bin_index=0):
+    ranks = pd.DataFrame(s).rank(method=rank_method, axis=1).values
+    return np.histogram(ranks, density=True, bins=bins)[0][bin_index]
+
+
+def calculate_rank_ci(s, nbootstrap=100, conf_level=0.9, ci_method="bca", bins=None, rank_method="first"):
+    
+    result = []
+    for bin_index, bin in enumerate(bins):
+        __prob_calc = partial(prob_calc, bins=bins, rank_method=rank_method, bin_index=bin_index)
+
+        result.append(
+            confidence_interval(__prob_calc, s, size=nbootstrap, ci_method=ci_method, cl=conf_level, bin_index=bin_index)
+        )
+
+    return result
+    
+
+
 def plot_ranks(lbl_to_data: dict, lbl_to_color: dict,
                leads=None,
                stid_to_name: dict = None,
                plot_params: dict = None,
-               img_dir: Path = None):
+               img_dir: Path = None,
+               alpha_ci=0.1,
+               nbootstrap=100,
+               rank_method="first"):
     """
     :param img_dir: directory to store plots
     :param lbl_to_data: label to model data (model data in the form of a dataframe)
@@ -88,6 +140,11 @@ def plot_ranks(lbl_to_data: dict, lbl_to_color: dict,
     """
 
     logger = log_utils.get_logger(__name__)
+
+
+
+
+    ppool = get_process_pool_executor()
 
     if stid_to_name is None:
         _stid_to_name = default_params.get_station_dict()
@@ -117,29 +174,82 @@ def plot_ranks(lbl_to_data: dict, lbl_to_color: dict,
 
     col_names = [io_manager.OBS_COL_NAME] + col_names
 
-    # calculate ranks
-    stid_to_lbl_to_data = {stid: OrderedDict() for stid in stid_list}
-    stid_to_lbl_to_data["all"] = OrderedDict()
-    for lbl, data in lbl_to_data.items():
-        logger.debug(data.head())
-        df = pd.DataFrame()
-        df[io_manager.OBS_COL_NAME] = data[col_names].rank(axis=1, method="first")[io_manager.OBS_COL_NAME]
-        df[io_manager.VALIDH_COL_NAME] = data[io_manager.VALIDH_COL_NAME]
-        df[io_manager.STID_COL_NAME] = data[io_manager.STID_COL_NAME]
-
-        for stid, rank in df.groupby(io_manager.STID_COL_NAME):
-            logger.debug("\n %s \n", rank.head())
-            stid_to_lbl_to_data[stid][lbl] = rank[[io_manager.OBS_COL_NAME, io_manager.VALIDH_COL_NAME]]
-
-        stid_to_lbl_to_data["all"][lbl] = df[[io_manager.OBS_COL_NAME, io_manager.VALIDH_COL_NAME]]
-
-    # nbins = number of members + 1
     nbins = len(col_names)
     bins = np.arange(0.5, nbins + 1.5, 1.)
     logger.debug("\nbins=%s\nlen(bins)=%s", bins, len(bins))
 
+
+
+    def __create_data_container():
+        dc = OrderedDict()
+        for lead_pair in leads:
+            dc[lead_pair] = {stid: OrderedDict() for stid in stid_list}
+            dc.update({"all": OrderedDict()})
+
+        return dc
+
+    # calculate ranks
+    stid_to_lbl_to_data = __create_data_container()
+   
+
+    from tqdm import tqdm
+                
+    
+    nbootstrap = 10
+
+    for lead_pair in leads:
+
+        for lbl, data in lbl_to_data.items():
+
+            logger.debug(data.head())
+
+            # select lead hours
+            vh = data[io_manager.VALIDH_COL_NAME]
+            data = data.loc[(vh >= lead_pair[0]) & (vh < lead_pair[1]), :]
+
+            df = pd.DataFrame()
+            df[io_manager.OBS_COL_NAME] = [prob_calc(data[col_names].values, bins=bins, bin_index=i) for i in range(len(bins))]
+            df["ci_min"] = np.nan
+            df["ci_max"] = np.nan
+
+            if nbootstrap > 0:
+
+                logger.info( "Data summary: \n%s\n",
+                    data[col_names].describe()
+                )
+
+                ci = calculate_rank_ci(data[col_names].values,
+                                    nbootstrap=nbootstrap, conf_level=1 - alpha_ci, ci_method="bca", bins=bins 
+                )
+
+                # progress bar   
+                ci = list(tqdm(ci,  total=len(df)))
+
+                logger.info(f"len ci 1 = {len(ci)}")
+                ci = list(zip(*ci))
+                logger.info(f"len ci 2 = {len(ci)}")
+
+
+                df["ci_min"] = ci[0]
+                df["ci_max"] = ci[1]
+            
+                    
+
+
+            df[io_manager.VALIDH_COL_NAME] = data[io_manager.VALIDH_COL_NAME]
+            df[io_manager.STID_COL_NAME] = data[io_manager.STID_COL_NAME]
+
+            for stid, rank in df.groupby(io_manager.STID_COL_NAME):
+                logger.debug("\n %s \n", rank.head())
+                stid_to_lbl_to_data[lead_pair][stid][lbl] = rank[[io_manager.OBS_COL_NAME, io_manager.VALIDH_COL_NAME, "ci_min", "ci_max"]]
+
+            stid_to_lbl_to_data[lead_pair]["all"][lbl] = df[[io_manager.OBS_COL_NAME, io_manager.VALIDH_COL_NAME, "ci_min", "ci_max"]]
+
+            logger.info(f"Processed {lbl} ...")
+
+    # nbins = number of members + 1
+   
     future_to_stid = {}
-    ppool = get_process_pool_executor()
     for stid in stid_to_lbl_to_data:
         rank_data = stid_to_lbl_to_data[stid]
         fut = ppool.submit(plot_ranks_for_station,
@@ -149,6 +259,6 @@ def plot_ranks(lbl_to_data: dict, lbl_to_color: dict,
     for a_future in as_completed(future_to_stid):
         stid = future_to_stid[a_future]
         fig = a_future.result()
-        img = img_dir / f"{stid}_{stname_to_fname2(_stid_to_name[stid])}.pdf"
+        img = img_dir / f"{stid}_{stname_to_fname2(_stid_to_name[stid])}.png"
         fig.savefig(img, bbox_inches="tight", transparent=True)
         plt.close(fig)
