e92fe5648cb7ba865158a6ba8013086b19ccba07
Oleksandr Huziy <oleksandr.huziy@ec.gc.ca>
2023-08-07 19:31:22 +0000

    Obs for the b2b plots are combined
    therefore it is important to remove duplicates

--- src/surge_validation/plot_timeseries_per_station.py
+++ src/surge_validation/plot_timeseries_per_station.py
@@ -168,7 +168,7 @@ def plot_time_series_for_station_many_models(swl_list, st_id, station_dict=defau
         st_sel_obs = st_sel_obs[st_sel_obs[io_manager.VALIDH_COL_NAME] >= min_valid_hour]
 
         st_sel_obs.sort_values([io_manager.TIME_COL_NAME, io_manager.VALIDH_COL_NAME], inplace=True)
-        # st_sel_obs.drop_duplicates(subset=io_manager.TIME_COL_NAME, keep="last", inplace=True)
+        st_sel_obs.drop_duplicates(subset=io_manager.TIME_COL_NAME, keep="last", inplace=True)
         st_sel_obs.set_index(io_manager.TIME_COL_NAME, inplace=True)
         st_sel_obs = st_sel_obs[io_manager.OBS_COL_NAME]
 
