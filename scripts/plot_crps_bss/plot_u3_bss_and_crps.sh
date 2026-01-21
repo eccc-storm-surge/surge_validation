06dbf1a11c857761070be35875f32c2c302cbb9b
Oleksandr Huziy <oleksandr.huziy@ec.gc.ca>
2026-01-21 17:26:30 +0000

    add launchers for u3: resps and gdsps

---
+++ scripts/plot_crps_bss/plot_u3_bss_and_crps.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -x
+data_root=~/R/resps_verification_plots/work/u3/
+
+declare  -A bss_plot_limits
+declare  -A crps_plot_limits
+
+bss_plot_limits["Surge_FS22"]="0 0.6"
+crps_plot_limits["Surge_FS22"]="0.03 0.09"
+bss_plot_limits["TWL_FS22"]="0.4 0.8"
+crps_plot_limits["TWL_FS22"]="0.075 0.2"
+
+# bss_plot_limits["Surge_ETE2019"]="0 0.6"
+# crps_plot_limits["Surge_ETE2019"]="0.03 0.09"
+# bss_plot_limits["TWL_ETE2019"]="0 1"
+# crps_plot_limits["TWL_ETE2019"]="0. 0.2"
+
+
+for season in FS22; do
+  for v in Surge TWL; do
+    common_root=$(ls -d ${data_root}/scratch_RESPS_${v}_${season}_REFvsRESPS_${v}_${season}_NEW_*)
+    python /home/olh001/Python/surge_validation/src/surge_validation/diagnostics/resps/plot_bss_and_crps.py \
+      --paths  ${common_root}/RESPS_${v}_${season}_REF \
+               ${common_root}/RESPS_${v}_${season}_NEW/ \
+      --labels "RESPS (${v}, REF)" "RESPS (${v}, NEW)" \
+      --colors b r --lead_hour_max 360 \
+      --out_dir ${data_root}/scratch_RESPS_${v}_${season}_REFvsRESPS_${v}_${season}_NEW_*/
+#      --bsslim ${bss_plot_limits["${v}_${season}"]} \
+#      --crpslim ${crps_plot_limits["${v}_${season}"]}
+  done
+done
+
