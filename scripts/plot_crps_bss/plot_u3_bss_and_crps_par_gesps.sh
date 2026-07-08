4efdb6196ebfefb88d722cbdae563472a2977c84
Oleksandr Huziy <oleksandr.huziy@ec.gc.ca>
2026-07-08 09:59:55 +0000

    ens: Updates to score plot

--- scripts/plot_crps_bss/plot_u3_bss_and_crps_par_gesps.sh
+++ scripts/plot_crps_bss/plot_u3_bss_and_crps_par_gesps.sh
@@ -18,13 +18,13 @@ crps_plot_limits["TWL_FS22"]="0.075 0.2"
 
 
 for v in TWL; do
-  common_root=$(ls -d ${data_root}/scratch_GESPS_${v}_REFvsGESPS_${v}_NEW_*)
+  common_root=$(ls -d ${data_root}/scratch_GESPS_${v}_REFvsGESPS_${v}_NEW_2026021900_2026031800)
   python /home/olh001/Python/surge_validation/src/surge_validation/diagnostics/resps/plot_bss_and_crps.py \
     --paths  ${common_root}/GESPS_${v}_REF \
               ${common_root}/GESPS_${v}_NEW/ \
     --labels "GESPS (${v}, REF)" "GESPS (${v}, NEW)" \
     --colors b r --lead_hour_max 72 \
-    --out_dir ${data_root}/scratch_GESPS_${v}_REFvsGESPS_${v}_NEW_*/ \
+    --out_dir ${common_root} \
     --obs-file /home/olh001/Python/obs_to_grid_mapping/gesps/gesps_global_obs_v3.0.0.obs \
     --skip-stations 9443090 3980 9449424 6485 6380
 done
