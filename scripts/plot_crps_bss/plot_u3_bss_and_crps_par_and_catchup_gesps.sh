853ec4ea395195f34e2bed0e8d334d9049e00130
Oleksandr Huziy <oleksandr.huziy@ec.gc.ca>
2026-07-08 10:21:35 +0000

    ens: add entry sctipt for gesps/u3

---
+++ scripts/plot_crps_bss/plot_u3_bss_and_crps_par_and_catchup_gesps.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -x
+data_root=~/R/resps_verification_plots/work/u3/gesps/par/
+
+declare  -A bss_plot_limits
+declare  -A crps_plot_limits
+
+bss_plot_limits["Surge_FS22"]="0 0.6"
+crps_plot_limits["Surge_FS22"]="0.03 0.09"
+bss_plot_limits["TWL_FS22"]="0.4 0.8"
+crps_plot_limits["TWL_FS22"]="0.075 0.2"
+
+# bss_plot_limits["Surge_ETE2019"]="0 0.6"
+# crps_plot_limits["Surge_ETE2019"]="0.03 0.09"
+# bss_plot_limits["TWL_ETE2019"]="0 1"
+# crps_plot_limits["TWL_ETE2019"]="0. 0.2"
+
+
+for v in TWL; do
+  common_root=$(ls -d ${data_root}/scratch_GESPS_${v}_REFvsGESPS_${v}_NEW_2026020600_2026031800)
+  python /home/olh001/Python/surge_validation/src/surge_validation/diagnostics/resps/plot_bss_and_crps.py \
+    --paths  ${common_root}/GESPS_${v}_REF \
+              ${common_root}/GESPS_${v}_NEW/ \
+    --labels "GESPS (${v}, REF)" "GESPS (${v}, NEW)" \
+    --colors b r --lead_hour_max 72 \
+    --out_dir ${common_root} \
+    --obs-file /home/olh001/Python/obs_to_grid_mapping/gesps/gesps_global_obs_v3.0.0.obs \
+    --skip-stations 3980 6485 6380 # 9449424 9443090 9452210 9437540 9440581 9441102 9440910
+done
+
